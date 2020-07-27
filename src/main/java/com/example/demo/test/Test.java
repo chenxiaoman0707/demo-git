8684b38975e046cd5af3aa2905eb6f20fc9b87a1
chenxiaoman <592799087@qq.com>
2020-07-27 15:27:06 +0800

    add in master

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) {
         String str = "hi";
         System.out.println("hello world");
-        System.out.println(str);
+        System.out.println(str+"resolve conflict");
     }
 
 }
