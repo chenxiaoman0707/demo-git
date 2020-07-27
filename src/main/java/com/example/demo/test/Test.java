01f2253511e0907243cad7f5475a439d0131b1a8
chenxiaoman <592799087@qq.com>
2020-07-27 15:05:27 +0800

    branch test

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -5,7 +5,9 @@ import java.sql.SQLOutput;
 public class Test {
 
     public static void main(String[] args) {
+        String str = "hi";
         System.out.println("hello world");
+        System.out.println(str);
     }
 
 }
