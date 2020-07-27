40dd5360cb9a90d2cfd72b36130fbe0e6112aec0
83582 <835822234@qq.com>
2020-07-27 17:42:12 +0800

    我修改了文件

--- src/main/java/com/example/demo/test/Fruit.java
+++ src/main/java/com/example/demo/test/Fruit.java
@@ -3,6 +3,6 @@ package com.example.demo.test;
 public class Fruit {
 
     public static void main(String[] args) {
-        System.out.println("apple");
+        System.out.println("chenmanyi ai chenzhibin");
     }
 }
