0624f3fb8e7e1b90d6cea816051e9ea9cc920d12
chenxiaoman <592799087@qq.com>
2020-07-29 11:38:30 +0800

    banana

--- src/main/java/com/example/demo/test/Fruit.java
+++ src/main/java/com/example/demo/test/Fruit.java
@@ -5,5 +5,6 @@ public class Fruit {
     public static void main(String[] args) {
         System.out.println("apple");
         System.out.println("feature1");
+        System.out.println("banana");
     }
 }
