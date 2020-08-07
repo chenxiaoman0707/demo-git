be4695d24ff3dc9e07578d2e6a9e041fb7c97d44
chenxiaoman0707 <592799087@qq.com>
2020-08-07 10:21:32 +0800

    测试一下

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -5,7 +5,7 @@ import java.sql.SQLOutput;
 public class Test {
 
     public static void main(String[] args) {
-        String str = "hi";
+        String str = "hi，test";
         System.out.println("hello world");
         System.out.println(str+"baby");
     }
